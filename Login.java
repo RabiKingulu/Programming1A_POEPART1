66c1d227061259e5f1b1c8a1582a51f5ce027847
RabiKingulu <ST10541087@rcconnect.edu.za>
2026-03-26 17:28:57 +0200

    Added variable declarations and validation methods

--- Login.java
+++ Login.java
@@ -1,7 +1,17 @@
-
-import java.util.*;public class Login
+import java.util.*;
+public class Login
 {
-	public static void main(String[]arg){
 		Scanner input = new Scanner(System.in);
+		String Username;
+		String Password;
+		String CellPhoneNumber;
+		Boolean checkUsername(String Username){
+			return Username.contains("_") && Username.length()<=5;
+		}
+	Boolean checkPasswordComplexity(String Password){
+		return Password.length()>=8 && Password.matches(".*[A-Z].*") && Password.matches(".*[0-9].*") && Password.matches(".*[^a-zA-Z0-9].*");
 	}
-}
+	Boolean checkCellPhoneNumber(String CellPhoneNumber){
+		return CellPhoneNumber.matches("^\\+27[0-9]{9}$");
+		}
+		}
